c8dc56f977a7d75c4a5c294f5cb5fa7a5c305e7c
Nikita <45867226+NikKram@users.noreply.github.com>
2021-03-07 17:16:45 -0700

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -21,13 +21,13 @@ contract Contract {
     Bidder[10] public bidders;
     
     uint public bidderCount = 0;
-    uint public runningEther0 = 0;
     uint public runningEther1 = 0;
+    uint public runningEther2 = 0;
 
     struct Bidder {
         uint betAmount;
         address payable bidderAddress;
-        bool vote;
+        uint8 vote;
     }
     
     constructor() {
@@ -39,31 +39,43 @@ contract Contract {
         _;
     }
     
-    function setWin(string memory _lastWin, bool _index, string memory _opt1, string memory _opt2) payable public onlyOwner {
+    function setWin(string memory _lastWin, uint8 _index, string memory _opt1, string memory _opt2) payable public onlyOwner {
         lastWin = _lastWin;
         option1 = _opt1;
         option2 = _opt2;
         
         uint winEther;
         uint lostEther;
-        if (_index==false) {
-            winEther =  runningEther0;
-            lostEther = runningEther1;
-        } else {
+        if (_index==1) {
             winEther =  runningEther1;
-            lostEther = runningEther0;
+            lostEther = runningEther2;
+        } else {
+            winEther =  runningEther2;
+            lostEther = runningEther1;
         }
 
         uint i;
         for (i = 0; i < bidderCount; i++) {
             if (bidders[i].vote == _index) {
-                bidders[i].bidderAddress.send(bidders[i].betAmount);  //returning original ether
-                bidders[i].bidderAddress.send(lostEther*(bidders[i].betAmount/winEther)); //profit payback
+                bidders[i].bidderAddress.transfer(bidders[i].betAmount);  //returning original ether
+                bidders[i].bidderAddress.transfer(lostEther*(bidders[i].betAmount/winEther)); //profit payback
             }     
         }
-
+    
+        reset(); //clean up
     }
     
+    function reset() private {
+        runningEther1 = 0;
+        runningEther2 = 0;
+        uint i;
+        for (i = 0; i < bidderCount; i++) { 
+                bidders[i].betAmount = 0; 
+                bidders[i].bidderAddress = 0x0000000000000000000000000000000000000000;
+                bidders[i].vote = 0;
+        }
+        bidderCount = 0;
+    }
     function bid(bool _index) public payable {
         require(msg.value > 0);
         if(bidderCount < bidders.length) {
@@ -71,11 +83,11 @@ contract Contract {
             bidders[bidderCount].bidderAddress = msg.sender;
             bidders[bidderCount].vote = _index;
             bidderCount++;
-            owner.send(msg.value);
+            owner.transfer(msg.value);
             if (_index == false) {
-                runningEther0+= msg.value;
-            } else {
                 runningEther1+= msg.value;
+            } else {
+                runningEther2+= msg.value;
             } 
         }
     }
